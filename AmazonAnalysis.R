b49ca58ccff13db42c5556bab00b32e0c7a98216
StacheXC <brettpedersen@icloud.com>
2024-10-07 15:02:28 -0600

    Did the first homework assignment

--- AmazonAnalysis.R
+++ AmazonAnalysis.R
@@ -1,8 +1,26 @@
 library(vroom)
 library(tidymodels)
 library(tidyverse)
+library(ggmosaic)
+library(embed)
 
 df = vroom("train.csv")
 df
 
-df$RESOURCE |> hist()
+ggplot(df, aes(x = ACTION)) + 
+  geom_bar()
+
+ggplot(df, aes(x = RESOURCE, y = as.factor(ACTION))) + 
+  geom_boxplot()
+
+my_recipe = recipe(ACTION ~ ., data = df) |> 
+  step_mutate_at(all_numeric_predictors(), fn = factor) |> 
+  step_other(all_predictors(), threshold = .001) |> 
+  step_dummy(all_nominal_predictors())
+
+prep = prep(my_recipe)
+baked = bake(prep, new_data = df)
+
+baked
+
+
